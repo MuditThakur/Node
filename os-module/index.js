4fc82eb992aa96a1fd0c9bccfc16f076b7e5cd57
MuditThakur <muditthakur10@gmail.com>
2024-07-04 15:27:04 +0530

    os-module

---
+++ os-module/index.js
@@ -0,0 +1,8 @@
+const os =require('os');
+console.log("the arch of comp is: " + os.arch());
+
+const freeMemory = (os.freemem()/(1024*1024*1024))
+console.log("free mem is : " + freeMemory)
+
+const totalMemory = (os.totalmem()/(1024*1024*1024))
+console.log("total mem is : " + totalMemory)
