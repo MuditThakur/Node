ef25cc506288e93590d31da1f610f4fc81db98b3
MuditThakur <muditthakur10@gmail.com>
2024-07-04 15:35:49 +0530

    os-module

--- os-module/index.js
+++ os-module/index.js
@@ -6,3 +6,6 @@ console.log("free mem is : " + freeMemory)
 
 const totalMemory = (os.totalmem()/(1024*1024*1024))
 console.log("total mem is : " + totalMemory)
+
+console.log("host name is : " + os.hostname())
+console.log("the type of os is : "+os.type())
